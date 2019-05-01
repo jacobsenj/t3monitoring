a545baeae1af36868302e2452870a20a9ff8d596
David König <david@hausformat.com>
2019-05-01 22:36:14 +0200

    [CLEANUP] Update ExtensionCategoryViewHelper for TYPO3 9LTS

--- Classes/ViewHelpers/Format/ExtensionCategoryViewHelper.php
+++ Classes/ViewHelpers/Format/ExtensionCategoryViewHelper.php
@@ -9,15 +9,14 @@ namespace T3Monitor\T3monitoring\ViewHelpers\Format;
  * LICENSE.txt file that was distributed with this source code.
  */
 
-use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface;
-use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
-use TYPO3\CMS\Fluid\Core\ViewHelper\Facets\CompilableInterface;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
 
 /**
  * Class ExtensionCategoryViewHelper
  */
-class ExtensionCategoryViewHelper extends AbstractViewHelper implements CompilableInterface
+class ExtensionCategoryViewHelper extends AbstractViewHelper
 {
     use CompileWithRenderStatic;
 
